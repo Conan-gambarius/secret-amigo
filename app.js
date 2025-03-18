31eacb72c615db50862e3fd4ae25965e243fb150
Conan-gambarius <antonio.soto.leon13@gmail.com>
2025-03-18 15:07:22 -0500

    Creación de Array, función de captura de nombres y función limpiar caja

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ function objetoYtexto(objeto, texto) {
 objetoYtexto('h1', 'Descubre tu amigo secreto');
 objetoYtexto('h2', 'Introduce los nombres de tus amigos');
 
+// Arreglo para almacenar participantes
+let friends = [];
+
+// Función que captura el valor del campo de entrada y lo agrega a la lista
+function capturaDeNombres() {
+    // Obtener el valor ingresado en el campo de texto
+    let nombresDeAmigos = document.getElementById('amigo').value;
+
+    // Validar que no esté vacío
+    if (nombresDeAmigos === "") {
+        alert('Por favor, inserte un nombre');
+        return; // Detiene la ejecución si el campo está vacío
+    }
+// Comprobar nombres duplicados
+    if (!friends.includes(nombresDeAmigos)) {
+    friends.push(nombresDeAmigos); // Agregar el nombre al array
+    } else {
+    alert('Este nombre ya fue agregado.');
+    }
+// Mostrar en la consola los siguientes elementos:
+    console.log(nombresDeAmigos);
+    console.log(friends);
+
+// Vaciar el campo de entrada después de digitar un nombre
+limpiarCaja();
+}
+
+// Función que limpia el campo de entrada
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
