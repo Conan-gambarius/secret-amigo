622aaf8a240cda8e6b39457cadd61b7c0ba6b4ec
Conan-gambarius <antonio.soto.leon13@gmail.com>
2025-03-18 14:52:05 -0500

    Función para modificar textos y cambios en textos

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Función que modifica el texto de un elemento HTML
+function objetoYtexto(objeto, texto) {
+    let objetoHTML = document.querySelector(objeto);
+    objetoHTML.innerHTML = texto;
+}
+
+// Cambios en los textos iniciales de la página
+objetoYtexto('h1', 'Descubre tu amigo secreto');
+objetoYtexto('h2', 'Introduce los nombres de tus amigos');
+
